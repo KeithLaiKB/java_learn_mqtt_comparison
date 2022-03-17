e9ceebb9236dac9d5fda516ba6e7aec098a3e6c9
KeithLaiKB <laidelivery@163.com>
2022-03-17 22:11:34 +0800

    update demo

--- mqtt_comparison1_workspace1/com.learn.mqtt.comparison.versionone/src/main/java/com/learn/mqtt/comparison/versionone/scenario1/hivemqttclient/publisher/TestMain_Hivemqmqttclient_Publisher.java
+++ mqtt_comparison1_workspace1/com.learn.mqtt.comparison.versionone/src/main/java/com/learn/mqtt/comparison/versionone/scenario1/hivemqttclient/publisher/TestMain_Hivemqmqttclient_Publisher.java
@@ -11,7 +11,6 @@ import com.hivemq.client.mqtt.mqtt5.message.connect.connack.Mqtt5ConnAck;
 import com.hivemq.client.mqtt.mqtt5.message.publish.Mqtt5PublishResult;
 /**
  * 
- * 
  * <p>
  * 							description:																				</br>	
  * &emsp;						qos 0																					</br>	
